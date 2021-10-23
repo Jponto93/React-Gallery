81eab2a6db745ffa9393b97f45f031553cb3f492
Jordan Ponto <jponto93@gmail.com>
2021-10-23 14:47:35 -0500

    love counter connects to DB and adds 1 like

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -9,17 +9,45 @@ const pool = require('../modules/pool.js')
 router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    // for(const galleryItem of galleryItems) {
+    //     if(galleryItem.id == galleryId) {
+    //         galleryItem.likes += 1;
+    //     }
+    // }
+
+    sqlText = `
+    UPDATE "galleryItems"
+    SET "likes" = "likes" + 1
+    WHERE "id" = $1;
+    `;
+    let values = [galleryId]
+
+    pool
+        .query(sqlText, values)
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('router.js error in PUT', err);
+        })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
+    console.log('inside router.js GET');
+    const sqlText =`
+    SELECT * FROM "galleryItems" ORDER BY "id" ASC;
+    `;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log('router.js successful GET', result.rows);
+        res.send(result.rows)
+    })
+    .catch((error) => {
+        console.log('router.js error in GET', error);
+        res.sendStatus(500);
+    })
+    
 }); // END GET Route
 
 module.exports = router;
