bcf70db220127fcede390ab59c665436ff6be5b9
Jordan Ponto <jponto93@gmail.com>
2021-10-24 15:10:32 -0500

    style added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,14 @@
 import Axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GalleryList from '../GalleryList/GalleryList';
 import GalleryForm from '../GalleryForm/GalleryForm';
-// import galleryItems from '../../../server/modules/gallery.data.js';
+
 
 
 function App() {
 
   let [galleryData, setGalleryData] = useState([]);
-  // let [isInEditMode, setIsInEditMode] = useState(false);
-  // let [galleryItemToEdit, setGalleryItemToEdit] = useState({});
-  
-
 
   // when page opens...
   useEffect(() => {
@@ -21,35 +17,16 @@ function App() {
 
   const fetchGallery = () => {
     Axios.get('/gallery')
-    .then((response) => {
-      console.log('successful GET', response.data);
-      setGalleryData(response.data)
-    }).catch((error) => {
-      console.log('Error in GET', error);
-    })
+      .then((response) => {
+        console.log('successful GET', response.data);
+        setGalleryData(response.data)
+      }).catch((error) => {
+        console.log('Error in GET', error);
+      })
   }; // fetchGallery
 
-
-  // const editGalleryItem = (galleryItemToEdit) => {
-  //   console.log(`about to edit something on the server with a put`);
-  //   console.log(`that object is`, galleryItemToEdit);
-  //   Axios({
-  //     method: `PUT`,
-  //     url: `/gallery/${galleryItemToEdit.id}`,
-  //     data: galleryItemToEdit
-  //   })
-  //     .then((response) => {
-  //       console.log('App PUT success');
-  //       fetchGallery();
-  //       setIsInEditMode(false);
-  //     })
-  //       .catch((error) => {
-  //         console.log('Error in App PUT', error);
-  //       })
-  // }
-
-    console.log(galleryData);
-    // console.log(`Is in edit mode:`, isInEditMode);
+  console.log(galleryData);
+  // console.log(`Is in edit mode:`, isInEditMode);
 
   const addToGallery = (newItem) => {
     console.log('Post start', newItem);
@@ -62,35 +39,30 @@ function App() {
         console.log('POST success');
         fetchGallery();
       })
-        .catch((error) => {
-          console.log('Error in post', error);
-        })
+      .catch((error) => {
+        console.log('Error in post', error);
+      })
   } // end addToGallery
 
-
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <GalleryForm 
-        addToGallery={addToGallery}
-        galleryData={galleryData}
-        fetchGallery={fetchGallery}
-        // setGalleryItemToEdit={setGalleryItemToEdit}
-        // galleryItemToEdit={galleryItemToEdit}
-        // setIsInEditMode={setIsInEditMode}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <div id="container">
+        <GalleryForm
+          addToGallery={addToGallery}
+          galleryData={galleryData}
+          fetchGallery={fetchGallery}
         />
-        <GalleryList 
-        galleryData={galleryData}
-        fetchGallery={fetchGallery}
-        // setGalleryItemToEdit={setGalleryItemToEdit}
-        // galleryItemToEdit={galleryItemToEdit}
-        // setIsInEditMode={setIsInEditMode}
+
+        <GalleryList
+          galleryData={galleryData}
+          fetchGallery={fetchGallery}
         />
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,23 +1,24 @@
 import axios from "axios";
 import { useState } from "react";
-import GalleryPhoto from "../GalleryPhoto/GalleryPhoto";
+import Button from '@mui/material/Button';
 
 
-function GalleryItem({ 
-    photo, 
+
+
+
+
+function GalleryItem({
+    photo,
     fetchGallery,
-    // setGalleryItemToEdit,
-    // setIsInEditMode,
-    // galleryItemToEdit
 }) {
-    
+
     let [flipToggle, setFlipToggle] = useState(true);
-    
-    function changeDisplay () {
+
+    function changeDisplay() {
         setFlipToggle(!flipToggle)
     } // changeDisplay
-    
-    function addLike () {
+
+    function addLike() {
         console.log('addLike clicked');
         console.log(photo.id);
         let id = photo.id;
@@ -28,9 +29,10 @@ function GalleryItem({
                 //refresh DOM
                 fetchGallery();
             })
-        
+
     } // end addLike
 
+    // this is what deletes pictures from the display
     const removeItem = () => {
         console.log('In remove item', photo.id);
         axios
@@ -43,39 +45,40 @@ function GalleryItem({
             })
     }
 
-    // const editItem = () => {
-    //     setIsInEditMode(true);
-    //     console.log(galleryItemToEdit);
-    //     // pass the current galleryItem as the galleryItemToEdit
-    //     setGalleryItemToEdit({...galleryItem });
-    // }
-
     return (
-        
+
         <>
             <div className="tile">
                 <div onClick={changeDisplay}>
-                    {flipToggle ? (<img className="imgToggle" src={photo.path} alt={photo.description}/> )  : 
-                    ( 
-                    <>
-                        {/* <button onClick={editItem}>Edit Description</button> */}
-                        <p className="imgToggle">
-                            {photo.description}
-                        </p> 
-                    </>)}
+                    {flipToggle ? (<img className="imgToggle" src={photo.path} alt={photo.description} />) :
+                        (
+                            <>
+                                {/* <button onClick={editItem}>Edit Description</button> */}
+                                <p className="imgToggle">
+                                    {photo.description}
+                                </p>
+                            </>)}
                 </div>
-                <div>
-                    <button className="loveBtn" onClick={addLike}>Love it</button>
-                    {photo.likes ? <p>{photo.likes} people love this!</p> : <p>No love yet!</p>}
-                </div>
-                <div>
-                    <button onClick={removeItem}>Remove</button>
+
+                <div className="container">
+
+                    <Button
+                        color="error"
+                        variant="text"
+                        size="small"
+                        onClick={addLike}>&#10084;</Button>
+
+                    {photo.likes ? <p>{photo.likes} People love this!</p> : <p>No love yet</p>}
+
+                    <Button
+                        color="warning"
+                        size="small"
+                        variant="text"
+                        onClick={removeItem}>&#9033;</Button>
                 </div>
             </div>
         </>
     )
-    
-
 
 }
 
